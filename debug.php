2a914edbf2335432ef96ea6cfc999eb6757ac42a
Ole Aass <oaass@php.net>
2015-11-25 10:49:51 +0100

    Two handy functions for debugging

---
+++ debug.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Analyze data
+ *
+ * var_dump()'s the content in $data to inspect and analyze the provided data
+ *
+ * @param mixed $data The data to be analyzed
+ * @param boolean $kill If all execution should be stopped
+ *
+ * @return void
+ */
+function analyzeData($data, $kill = true)
+{
+    var_dump($data);
+    if ($kill) { die(); }
+}
+
+/**
+ * Trace execution
+ *
+ * Usage:
+ *  tracer('init');
+ * 
+ * Output:
+ *  sTracer: Check point #1 (init)
+ *
+ * @param string $info Add a message to the check point
+ *
+ * @return void
+ */
+function tracer($info = '', $reset = false)
+{
+    static $counter = 0;
+    $counter = ($reset) ? 0 : $counter+1;
+    echo "<strong>Tracer:</strong> Check point #{$counter} ({$info})<br>";
+}
